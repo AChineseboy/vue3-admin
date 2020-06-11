b9810a704dc757e942bd11d2fd5c2cd927466b64
AChineseboy <fhm011101@126.com>
2020-06-11 14:28:16 +0800

    refactor: directory structure adjustment

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Layout from '../layout/index.vue';
+import HelloWord from '../views/HelloWord.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: Layout,
+    children: [
+      {
+        path: '/helloword',
+        component: HelloWord,
+      },
+    ],
   },
 ];
 
