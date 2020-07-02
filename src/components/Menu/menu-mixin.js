77aa316d6320745b926c8f5026a569173fbf613f
AChineseboy <fhm011101@126.com>
2020-07-02 16:34:51 +0800

    fix: fix paddingStyle

--- src/components/Menu/menu-mixin.js
+++ src/components/Menu/menu-mixin.js
@@ -4,8 +4,14 @@ export default {
     paddingStyle() {
       let padding = 20;
       let parent = this.$parent;
-      while (parent.$options.name !== 'ZmMenu') {
-        padding += 20;
+      console.log(parent, parent.$options, this.$options.name);
+      while (parent && parent.$options.name !== 'ZmMenu') {
+        if (parent.$options.name === 'ZmSubmenu') {
+          padding += 20;
+        }
+        // if (parent.$parent === null) {
+        //   console.log(parent.$options);
+        // }
         parent = parent.$parent;
       }
       return {
